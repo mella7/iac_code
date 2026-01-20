80922229ad39c622097002bc07fda0423114e31e
mella7 <mellahmohamedali@gmail.com>
2026-01-20 19:07:40 +0100

    fmt terraform

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
     }
   }
 
   backend "s3" {
     bucket = "mella7-bucket-01"
-    key = "terraform/terraform.tfstate"
+    key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,44 @@
 variable "region" {
   description = "AWS region"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc-name" {
   description = "VPC Name for our Jumphost server"
-  type = string 
-  default = "Jumphost-vpc"
+  type        = string
+  default     = "Jumphost-vpc"
 }
 
 variable "igw-name" {
   description = "Internet Gate Way Name for our Jumphost server"
-  type = string
-  default = "Jumphost-igw"
+  type        = string
+  default     = "Jumphost-igw"
 }
 
 variable "subnet-name" {
   description = "Subnet Name for our Jumphost server"
-  type = string
-  default = "Jumphost-subnet"
+  type        = string
+  default     = "Jumphost-subnet"
 }
 
 variable "rt-name" {
   description = "Route Table Name for our Jumphost server"
-  type = string
-  default = "Jumphost-rt"
+  type        = string
+  default     = "Jumphost-rt"
 }
 
 variable "sg-name" {
   description = "Security Group for our Jumphost server"
-  type = string
-  default = "Jumphost-sg"
+  type        = string
+  default     = "Jumphost-sg"
 }
 
 
 variable "iam-role" {
   description = "IAM Role for the Jumphost Server"
-  type = string
-  default = "Jumphost-iam-role"
+  type        = string
+  default     = "Jumphost-iam-role"
 }
 
 /*
@@ -58,7 +58,7 @@ variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
   default     = "ami-0724302e25d16f8f2" // Replace with the latest AMI ID for your region
-  
+
 }
 
 variable "instance_type" {
